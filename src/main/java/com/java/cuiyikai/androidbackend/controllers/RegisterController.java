da885a90ddaa9c7558bd6d9dc3e4c57457fb2f49
cuibst <cbst987@126.com>
2021-09-08 16:37:55 +0800

    Login controller documented

--- src/main/java/com/java/cuiyikai/androidbackend/controllers/RegisterController.java
+++ src/main/java/com/java/cuiyikai/androidbackend/controllers/RegisterController.java
@@ -18,6 +18,10 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * {@link Controller} for register-related apis.
+ * Mapped to url {@code "/api/register"}
+ */
 @Controller
 @RequestMapping("/api/register")
 public class RegisterController {
@@ -32,8 +36,27 @@ public class RegisterController {
 
     private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
 
+    /**
+     * <p>Receive user's register information and register if possible.</p>
+     * <p>If success, backend will send a validate email to user's email address.</p>
+     * <p>Map to url {@code "/"}</p>
+     * <p>Use {@link RequestMethod#POST} method</p>
+     * <p>Will return a {@link JSONObject} with status and message.</p>
+     * @param jsonParam A {@link JSONObject} of register parameters, should contains following keys.
+     *                  <p>
+     *                  "username" : username of the user.
+     *                  "password" : password of the user.
+     *                  "email"    : email address of the user.
+     *                  both values should not be empty, and username should not be same as any user existing in the database.
+     *                  </p>
+     * @param response A {@link HttpServletResponse}, see {@link PostMapping}.
+     * @throws IOException see {@link PostMapping}
+     */
     @PostMapping("/")
     public void registerNewUser(@RequestBody JSONObject jsonParam, HttpServletResponse response) throws IOException {
+
+        //Phase 1: Check request parameters.
+
         String username = jsonParam.getString("username");
         String password = jsonParam.getString("password");
         String email = jsonParam.getString(NetworkUtilityClass.PARAMETER_EMAIL);
@@ -57,7 +80,8 @@ public class RegisterController {
             printWriter.print(reply);
             return;
         }
-        userServices.registerNewUser(username, password, email);
+
+        //Phase 2: send the validation email.
         String validateToken = tokenServices.insertNewToken(username);
         HtmlEmail email1 = new HtmlEmail();
         email1.setHostName("smtp.126.com");
@@ -79,12 +103,27 @@ public class RegisterController {
             printWriter.print(reply);
             return;
         }
+
+        //Phase 3: register the user.
+        userServices.registerNewUser(username, password, email);
+
+        //Phase 4: reply success.
+
         JSONObject reply = new JSONObject();
         reply.put(NetworkUtilityClass.PARAMETER_STATUS, NetworkUtilityClass.STATUS_OK);
         reply.put(NetworkUtilityClass.PARAMETER_MESSAGE, "register successfully");
         printWriter.print(reply);
     }
 
+    /**
+     * <p>Check the user's validate email, and validate the user.</p>
+     * <p>Use {@link RequestMethod#GET} and map to url {@code "/check"}</p>
+     * <p>Reply a {@link JSONObject} with status and message.</p>
+     * @param token user's validate token, should be same with the one when the user is registered.
+     * @param username user's username. This should match with the token.
+     * @param response A {@link HttpServletResponse}, see {@link GetMapping}.
+     * @throws IOException see {@link GetMapping}
+     */
     @GetMapping("/check")
     public void checkToken(@RequestParam String token, @RequestParam String username, HttpServletResponse response) throws IOException {
         response.setHeader(NetworkUtilityClass.CONTENT_TYPE, NetworkUtilityClass.JSON_CONTENT_TYPE);
@@ -123,6 +162,14 @@ public class RegisterController {
         printWriter.print(reply);
     }
 
+    /**
+     * <p>Check whether there is a collided username</p>
+     * <p>Use {@link RequestMethod#GET} method and map to url {@code "/check/username"}</p>
+     * <p>Reply a {@link JSONObject} only contains status</p>
+     * @param username the username being checked
+     * @param response A {@link HttpServletResponse}, see {@link GetMapping}.
+     * @throws IOException see {@link GetMapping}
+     */
     @GetMapping("/check/username")
     public void checkUsername(@RequestParam String username, HttpServletResponse response) throws IOException {
         logger.info("Checking username {}", username);
